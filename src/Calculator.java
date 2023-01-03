63ac2dfb46c3d0bc50e0c01d7ff16e560d511d43
Alexandra <84934027+Blootooz@users.noreply.github.com>
2023-01-03 21:59:54 +0300

    fix

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,15 +6,17 @@ public class Calculator {
         BinaryOperator<Integer> plus = (x, y) -> x + y;
         BinaryOperator<Integer> minus = (x, y) -> x - y;
         BinaryOperator<Integer> multiply = (x, y) -> x * y;
-        BinaryOperator<Integer> devide = (x, y) -> x / y;///BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0;
+        BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0;
 
         UnaryOperator<Integer> pow = x -> x * x;
-        UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;///UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+        UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
         Predicate<Integer> isPositive = x -> x > 0;
 
-        Consumer<Integer> println = System.out::println;
+    Consumer<Integer> println = System.out::println;
 
         }
+
+
 
 
